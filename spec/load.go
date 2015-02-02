99138728ed9fb8d9f65855c071900f4e7091c802
George Lesica <george@lesica.com>
2015-02-02 13:47:26 -0300

    Format changes.

--- spec/load.go
+++ spec/load.go
@@ -1,27 +1,27 @@
 package spec
 
 import (
-    "encoding/json"
-    "io/ioutil"
+	"encoding/json"
+	"io/ioutil"
 )
 
 func LoadJsonFile(filename string) *Spec {
-    raw, err := ioutil.ReadFile(filename)
-    if err != nil {
-        panic(err)
-    }
+	raw, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
 
-    return loadJsonString(raw)
+	return loadJsonString(raw)
 }
 
 func loadJsonString(content []byte) *Spec {
-    spec := new(Spec)
-    err := json.Unmarshal(content, spec)
-    if err != nil {
-        panic(err)
-    }
+	spec := new(Spec)
+	err := json.Unmarshal(content, spec)
+	if err != nil {
+		panic(err)
+	}
 
-    spec.setDefaults()
+	spec.setDefaults()
 
-    return spec
+	return spec
 }
